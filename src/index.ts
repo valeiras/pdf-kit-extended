68e1fe0eafd323300d1fe87dd7e31f846db5ff88
David Reverter Valeiras <d.r.valeiras@gmail.com>
2024-03-18 12:25:42 +0100

    Adds types exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { default as PdfKitExtended } from "./PdfKitExtended.ts";
 // import { PDFDocument } from "pdfkit";
 export { default as PDFKit } from "pdfkit";
+
+export * from "./types.ts";
